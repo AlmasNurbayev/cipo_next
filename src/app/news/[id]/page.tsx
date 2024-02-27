f7bbf8d75603e213cd81c3370bd7724d43cf184e
almas <ntldr@mail.ru>
2024-02-27 15:52:51 +0600

    fix style in news:id

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -2,35 +2,39 @@ import { cipoGetNews } from '@/api/cipo.api';
 import { config } from '@/config/constants';
 import { Metadata } from 'next';
 import Image from 'next/image';
-import 'server-only'
+import 'server-only';
+import './page.css';
 
 type Props = {
-  params: { id: string }
+  params: { id: string };
   // searchParams: { [key: string]: string | string[] | undefined }
-}
+};
 
-export async function generateMetadata(
-  { params }: Props,
-): Promise<Metadata> {
-  const product = (await cipoGetNews(params.id)).data
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const product = (await cipoGetNews(params.id)).data;
   return {
     title: 'Новости / ' + product.title,
-  }
+  };
 }
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function NewPage({ params }: { params: { id: string } }) {
   const news = (await cipoGetNews(params.id)).data;
+
   return (
-    <main className="main">
-      <div>id : {params.id}</div>
-      <h1>title: {news.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: news.data}}></div>
-      <Image
-        src={config.backendUrl + '/' + news.image_path}
-        width={300}
-        height={300}
-        alt={news.title}
-      />
+    <main className="newPage _container">
+      <div className="title">
+        <h1>{news.title}</h1>
+      </div>
+
+          <Image
+            src={config.backendUrl + '/' + news.image_path}
+            width={300}
+            height={300}
+            alt={news.title}
+          />
+          <div dangerouslySetInnerHTML={{ __html: news.data }}></div>
+
+
     </main>
   );
 }
