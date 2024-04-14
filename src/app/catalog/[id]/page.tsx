d65e128d350ef687c3a9918189d9d6199639b043
almas <ntldr@mail.ru>
2024-04-14 12:45:48 +0500

    (catalog:id) change price section

--- src/app/catalog/[id]/page.tsx
+++ src/app/catalog/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import 'server-only';
 import './page.css';
 import Script from 'next/script';
+import { config } from '@/config/constants';
 
 type Props = {
   params: { id: string };
@@ -12,16 +13,36 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const product = (await cipoGetProduct(params.id)).data;
+
+  const images = [];
+  for (const image of product.image_registry) {
+    if (image.main) {
+      images.push({
+        url: config.staticPathPublic + '/' + image.full_name,
+        width: 700,
+        height: 700,
+      });
+    }
+  }
+
   return {
     title: product.name,
+    openGraph: {
+      title: product.name,
+      //description: 'The React Framework for the Web',
+      url: config.frontUrl + '/catalog/' + params.id,
+      siteName: 'Cipo.kz',
+      images,
+      locale: 'ru_RU',
+      type: 'website',
+    },
   };
 }
 
 export default async function GoodPage({ params }: { params: { id: string } }) {
-  
   const product = (await cipoGetProduct(params.id)).data;
   const stores = (await cipoListStores()).data;
-        // @refresh reset
+  let active;
 
   return (
     <main className="good _container">
@@ -74,59 +95,67 @@ export default async function GoodPage({ params }: { params: { id: string } }) {
           <div className="nalichie">Наличие и цена в магазинах:</div>
 
           <div className="tab-nav">
-            <>
-              {product.qnt_price_registry_group.map((e, index) => (
-                <a
-                  className="tab-link"
-                  href={'#size' + e.size_name_1c}
-                  key={'a' + e.size_name_1c}
+            {product.qnt_price_registry_group.map((e, index) => (
+              <>
+                <input
+                  type="radio"
+                  id={'accordion-' + index}
+                  className="accordion-checkbox"
+                  name="size_accordition"
+                />
+                <label
+                  id={'accordion-' + index + '-header'}
+                  className="accordion-header"
+                  htmlFor={'accordion-' + index}
                 >
-                  {e.size_name_1c}
-                </a>
-              ))}
-            </>
+                  <h4>{'размер: ' + e.size_name_1c + ', цена: ' + e.sum.toLocaleString('ru-RU') + ' ₸'}</h4> 
+                </label>
+                <div className="accordion-content">
+                  <div
+                    id={'size' + e.size_name_1c}
+                    className="right__title"
+                    key={'size' + e.size_name_1c}
+                    title={e.size_name_1c}
+                  >
+                    <div className="right__store">
+                      <ul key={'store' + e.size_name_1c}>Магазин:</ul>
+                      {e.store_id.map((store_id) => (
+                        <li key={'store_id' + store_id}>
+                          {stores.find((store) => store.id === store_id)?.address}
+                        </li>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
           </div>
-          {product.qnt_price_registry_group.map((e, index) => (
-            <div
-              id={'size' + e.size_name_1c}
-              className="right__title tab-on tab-none"
-              key={'size' + e.size_name_1c}
-              title={e.size_name_1c}
-            >
-              <div className='right__size'>размер: {e.size_name_1c}</div>
-              <div className="right__price">
-                <h2>{e.sum.toLocaleString('ru-RU') + ' ₸'}</h2>
-              </div>
-              <div className="right__store">
-                {/* <div key={'p' + e.size_name_1c}>
-                    
-                  </div> */}
-                <ul key={'store' + e.size_name_1c}>Магазин:</ul>
-                {e.store_id.map((store_id) => (
-                  <li key={'store_id' + store_id}>
-                    {stores.find((store) => store.id === store_id)?.address}
-                  </li>
-                ))}
-              </div>
-            </div>
-          ))}
         </div>
       </div>
-      <Script id="links" strategy='beforeInteractive'>
-        {`const links = document.querySelectorAll('a');
-          console.log(links.length);
-          for (let i = 0; i < links.length; i++) {
-            if (links[i].href.includes('#size') === false) {
-              continue;
-            }
-            links[i].addEventListener('click', (e) => {
-                for (let j = 0; j < links.length; j++) {
-                  links[j].classList.remove('active_size');
+      <Script id="links" strategy="beforeInteractive">
+        {`
+          // скрипт работает только при первом открытии страницы
+          // при навигации между товарами он уже НЕ РАБОТАЕТ
+          const inputs = document.querySelectorAll('label');
+          console.log(inputs.length);
+          for (let i = 0; i < inputs.length; i++) {
+
+            inputs[i].addEventListener('click', (e) => {
+                for (let j = 0; j < inputs.length; j++) {
+                  inputs[j].classList.remove('accordition-header-active');
                 }
-               links[i].classList.add('active_size');
+                inputs[i].classList.add('accordition-header-active');
 
             })
           }
+
+          window.addEventListener('load', function () {
+            const firstInput = document.querySelector(".accordion-checkbox");
+            // Установим его checked в true
+            if (firstInput) {
+              firstInput.checked = true;
+            }
+          })
         `}
       </Script>
     </main>
